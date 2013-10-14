52ae6ba350ecca59d6a4ca46bd31e031e93ca6a3
Markus Ziegler <mziegler.itsb-b2013@fh-salzburg.ac.at>
2013-10-14 15:27:49 +0200

    INITIAL COMMIT

---
+++ UEB_GIT_INIT/src/UEB_GIT_INIT.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UEB_GIT_INIT.c
+ Author      : Markus Ziegler
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello GIT!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
